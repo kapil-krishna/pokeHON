d20047d4c6ecbc8576bd7abc26963c354ffe44a4
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-05-28 18:39:41 +0100

    Commit before making dependency changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,17 @@
 import controllers.PokemonController;
 import models.db.PokemonDBModel;
 import models.pokeAPI.PokemonResponseModel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import services.PokeAPIService;
 import services.PokemonService;
 
 import java.util.List;
 
+@SpringBootApplication
 public class Main {
 
     public static void main (String[] args) {
-        PokemonService pokemonService = new PokemonService();
-        PokeAPIService pokeAPIService = new PokeAPIService();
-        PokemonController pokemonController = new PokemonController(pokemonService, pokeAPIService);
-
-        pokemonController.fetchPokemonData(5);
-
-//        PokeAPIService pokeAPIService = new PokeAPIService();
-//
-//        PokemonResponseModel pokemon = pokeAPIService.getPokemonFromApi(1);
-//
-//        System.out.println(pokemon.getName());
-
+        SpringApplication.run(Main.class, args);
     }
 }
